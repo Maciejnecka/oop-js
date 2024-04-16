4e65ab9240bb3a38de9f8f125e9b5d45c2b1f82d
Maciej Necka <macieknecka@gmail.com>
2024-04-16 10:01:36 +0200

    03.01-20

---
+++ 03/03-03.js
@@ -0,0 +1,12 @@
+function Checker(color, player) {
+  this.color = color;
+  this.player = player;
+  this.moves = [];
+}
+
+const checker = new Checker('white', 'Anna');
+
+const { moves, player: pName } = checker;
+moves.push('61-52');
+
+console.log(moves, pName, checker.moves); //['61-52'] 'Anna'
---
+++ 03/03-13.js
@@ -0,0 +1,14 @@
+const checker = {
+  // color: 'white',
+  player: 'Anna',
+  moves: [],
+};
+
+Object.defineProperty(checker, 'color', {
+  value: 'white',
+  writable: true,
+  enumerable: false,
+  configurable: true,
+});
+
+console.log(Object.keys(checker));
